7bbd35d05f70ec0e45ce4569b10a13c967550b35
Hanaa Sadoqi <hsadoqi@gmail.com>
2020-03-03 21:31:38 -0500

    Initial Commit

---
+++ Fake Fake News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fake Fake News
+//
+//  Created by Hanaa Sadoqi on 3/3/20.
+//  Copyright © 2020 Hanaa Sadoqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
